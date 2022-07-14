7277376d412728432948deeb3942a8f83bd260a6
Jatterai <maiorova.yuna@gmail.com>
2022-07-14 19:06:30 +0700

    debugging

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-	let open_brackets = bracketsConfig.map(item => item[0]);
-	let bracket_pair = {};
+	let open_brackets = bracketsConfig.filter(item => (item[0] !== item[1])).map(item => item[0]); let shit = bracketsConfig.filter(item => (item[0] === item[1])).map(item => item[0]);
 
+	let bracket_pair = {};
 	for (let config of bracketsConfig) {
 		bracket_pair[config[1]] = config[0];
 	}
 
 	let stack = [];
-
 	for (let bracket of str) {
 		if (open_brackets.includes(bracket)) {
 			stack.push(bracket);
-		} else if (!stack.length) {
+		} else if (!stack.length && bracket !== bracket_pair[bracket]) {
 			return false;
 		}
-
 		let last_open_bracket = stack[stack.length - 1];
 
-		if (bracket_pair[bracket] === bracket) {
-			if (stack.length === 1) {
-				continue;
+		if (shit.includes(bracket)) {
+			if (bracket !== last_open_bracket || !stack.length) {
+				stack.push(bracket);
 			} else {
-				if (last_open_bracket === stack[stack.length - 2]) {
-					stack.pop();
-					stack.pop();
-				}
+				stack.pop();
+				continue;
 			}
 		}
 		if (bracket_pair[bracket] === last_open_bracket) {
 			stack.pop();
 		}
 	}
+
 	return !stack.length
 }
+
